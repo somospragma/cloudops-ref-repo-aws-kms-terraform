b4cc978c5b98891195ff200c1d84dba2b3b22737
farciniegas52 <felipe.arciniegas@pragma.com.co>
2025-06-12 20:47:35 -0500

    Init Module KMS

--- outputs.tf
+++ outputs.tf
@@ -35,3 +35,12 @@ output "kms_complete_info" {
     }
   }
 }
+
+
+output "debug_info" {
+  value = {
+    caller_account = data.aws_caller_identity.current.account_id
+    caller_arn     = data.aws_caller_identity.current.arn
+    caller_user_id = data.aws_caller_identity.current.user_id
+  }
+}
